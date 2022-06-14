6c7b15121f349c45e14e9c4dc6145c874ebc7a09
Clara <107491776+clara-aralc@users.noreply.github.com>
2022-06-14 18:24:56 -0300

    Add files via upload

---
+++ formulário.js
@@ -0,0 +1,15 @@
+function hehe(){
+
+    var input =prompt("Informe um número: ")
+    if (input <= 1){
+        alert("Que tal colocar como meta ler mais?")
+    }
+    else{
+        if (input <= 10){
+            alert("Parabéns!")
+        }
+        else{
+            alert("UAU!! Você leu muito mesmo!")
+        }
+    }    
+}
